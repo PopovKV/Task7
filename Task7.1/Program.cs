72583eaed24c52e40f7c5b6906dfa9228b9408ad
Кирилл Попов <kirill.kiria@yandex.ru>
2022-02-27 16:28:12 +0300

    Добавьте файлы проекта.

---
+++ Task7.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+                            Area(a, b, c, out double S1);
+                            Area(a1, b1, c1, out double S2);
+            if (S1>S2)
+            {
+                Console.WriteLine("Площадь 1 треугольника больше");
+            }
+            else
+            {
+                if (S2>S1)
+                {
+                    Console.WriteLine("Площадь 2 треугольника больше");
+                }
+                else
+                {
+                    Console.WriteLine("Площади трегольников равны");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        static void Area(int a, int b, int c, out double S)
+        {
+            double p = (a + b + c) / 2;
+            S = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+        }
+    }
+}
---
+++ Task7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Cube(a);
+            Console.ReadKey();
+        }
+        static void Cube(int a)
+        {
+            
+            double S = 6 * a * a;
+            double V = a * a * a;
+            Console.WriteLine(S);
+            Console.WriteLine(V);
+        }
+
+    }
+}
